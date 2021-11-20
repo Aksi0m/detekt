f237c58b14443951efdf15d1737abe1d0d39cea1
Brais <braisgabin@gmail.com>
2021-11-20 20:47:28 +0100

    Concat configurations

--- detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/DefaultConfigProvider.kt
+++ detekt-core/src/main/kotlin/io/gitlab/arturbosch/detekt/core/tooling/DefaultConfigProvider.kt
@@ -2,10 +2,12 @@ package io.gitlab.arturbosch.detekt.core.tooling
 
 import io.github.detekt.tooling.api.DefaultConfigurationProvider
 import io.github.detekt.tooling.api.spec.ProcessingSpec
-import io.github.detekt.tooling.internal.getSafeResourceAsStream
-import io.github.detekt.tooling.internal.openSafeStream
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.core.config.YamlConfig
+import io.gitlab.arturbosch.detekt.core.settings.ExtensionFacade
+import java.io.InputStream
+import java.io.PipedInputStream
+import java.io.PipedOutputStream
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
@@ -20,15 +22,36 @@ class DefaultConfigProvider : DefaultConfigurationProvider {
     override fun get(): Config = spec.getDefaultConfiguration()
 
     override fun copy(targetLocation: Path) {
-        val configUrl = javaClass.getResource(DEFAULT_CONFIG_RESOURCES_PATH)!!
-        Files.copy(configUrl.openSafeStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING)
+        Files.copy(configInputStream(spec), targetLocation, StandardCopyOption.REPLACE_EXISTING)
     }
 }
 
-fun ProcessingSpec.getDefaultConfiguration(): Config {
-    return requireNotNull(javaClass.getSafeResourceAsStream(DEFAULT_CONFIG_RESOURCES_PATH))
-        .reader()
-        .use(YamlConfig::load)
+private fun configInputStream(spec: ProcessingSpec): InputStream {
+    val outputStream = PipedOutputStream()
+
+    Thread {
+        requireNotNull(spec.javaClass.getResourceAsStream("/default-detekt-config.yml"))
+            .use { it.copyTo(outputStream) }
+
+        ExtensionFacade(spec.extensionsSpec).pluginLoader
+            .getResourcesAsStream("config/config.yml")
+            .forEach { inputStream ->
+                outputStream.bufferedWriter().append('\n').flush()
+                inputStream.use { it.copyTo(outputStream) }
+            }
+
+        outputStream.close()
+    }.start()
+
+    return PipedInputStream(outputStream)
 }
 
-private const val DEFAULT_CONFIG_RESOURCES_PATH = "/default-detekt-config.yml"
+private fun ClassLoader.getResourcesAsStream(name: String): Sequence<InputStream> {
+    return getResources(name)
+        .asSequence()
+        .map { it.openStream() }
+}
+
+fun ProcessingSpec.getDefaultConfiguration(): Config {
+    return YamlConfig.load(configInputStream(this).reader())
+}
